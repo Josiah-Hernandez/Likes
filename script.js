d63768840a38982bb429a4dc5a09919241f12891
Josiah-Hernandez <jhern88@me.com>
2022-05-24 23:53:47 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function numberoflikes1() {
+    document.querySelector("#likes1").innerText++;
+}
+
+function numberoflikes2() {
+    document.querySelector("#likes2").innerText++;
+}
+
+function numberoflikes3() {
+    document.querySelector("#likes3").innerText++;
+}
